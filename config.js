54b446937dac57544222db2fe53ff291f013678b
mcquinn9395 <90846837+mcquinn9395@users.noreply.github.com>
2021-09-16 20:31:45 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,16 +2,16 @@ const CONFIG = {
     introTitle: 'Em bé Unicorn ơi!',
     introDesc: `Gặp gỡ nhau muộn chút vẫn tốt chứ sao.
     Chị đúng độ vào quãng đời chín chắn.
-    Còn em thì bước sang tuổi đủ dịu dàng`,
-    btnIntro: 'Chị muốn nói là...',
+    Còn em thì bước sang tuổi đủ dịu dàng.`,
+    btnIntro: 'Chị muốn nói với em là...',
     title: 'Em ơi, yêu là không lo mà đã lo là không yêu',
     desc: 'Thế mình yêu nhau đi và đừng lo gì cả nhé',
     btnYes: 'Cứ tưởng Quỳnh không bao giờ nói ra',
     btnNo: 'Quỳnh mơ đấy à',
-    question:'Đùa tí! Nhưng thích em là thật',
+    question:'Đùa tí! Nhưng thích em là thật <3',
     btnReply: 'Em đoán được chị sắp nói gì rồi phải không?',
-    reply: 'Vừa giỏi giang, vừa đáng tin, vừa sâu sắc, vừa có 1 chút nổi loạn, và nhìu phần đáng iu',
-    mess: 'Chị ko ngại đợi, nhưng ko biết phải đợi đến khi nào để được gặp em, cũng ko muốn bỏ lỡ cơ hội',
+    reply: 'Vừa giỏi giang, vừa đáng tin, vừa sâu sắc, có một chút nổi loạn, và nhìu phần đáng iu',
+    mess: 'Chị không ngại đợi, nhưng không biết phải đợi đến khi nào để được gặp em, cũng không muốn bỏ lỡ cơ hội',
     messDesc: 'Nếu không phải là em thì sẽ là ai được đây',
     btnAccept: 'Thảo Vy, em có muốn hẹn hò với chị không? Nói cho chị nghe nhé',
     messLink: 'https://www.instagram.com/mcquinn9395/' //link mess của các bạn. VD: https://m.me/nam.nodemy
