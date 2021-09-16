003c9189cf25c13fb27f7bdd3cd0410ca8f3e2a8
mcquinn9395 <90846837+mcquinn9395@users.noreply.github.com>
2021-09-16 20:52:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,14 +5,14 @@ const CONFIG = {
     Còn em thì bước sang tuổi đủ dịu dàng.`,
     btnIntro: 'Chị muốn nói với em là...',
     title: 'Em ơi, yêu là không lo mà đã lo là không yêu',
-    desc: 'Thế mình yêu nhau đi và đừng lo gì nhé',
+    desc: 'Thế mình yêu nhau đi và đừng lo gì cả nhé',
     btnYes: 'Cứ tưởng Quỳnh không bao giờ nói ra',
-    btnNo: 'Quỳnh mơ đấy à',
-    question:'Đùa tí! Nhưng thích em là thật <3',
+    btnNo: 'Ơ Quỳnh mơ đấy à',
+    question:'Đùa tí! Nhưng thích em là thật <3 Muốn biết vì sao thì em gõ vào ô trắng bên dưới nhé',
     btnReply: 'Em đoán được chị sắp nói gì rồi phải không?',
     reply: 'Vừa giỏi giang, vừa đáng tin, vừa sâu sắc, có một chút nổi loạn, và nhìu phần đáng iu',
     mess: 'Chị không ngại đợi, nhưng không biết phải đợi đến khi nào để được gặp em, cũng không muốn bỏ lỡ cơ hội',
     messDesc: 'Nếu không phải là em thì sẽ là ai được đây',
-    btnAccept: 'Thảo Vy, em có muốn hẹn hò với chị không? Nói cho chị nghe nhé',
+    btnAccept: 'Thảo Vy, em có muốn hẹn hò với chị không? Nói cho chị nghe với nhé',
     messLink: 'https://www.instagram.com/mcquinn9395/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
